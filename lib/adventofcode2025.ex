5d7b3101e8a8de13ad0dcfae4865d5360f5556d8
Rune Moennike <runemoennike@gmail.com>
2025-11-25 22:17:03 +0100

    Initial mix setup.

---
+++ lib/adventofcode2025.ex
@@ -0,0 +1,20 @@
+defmodule Adventofcode2025 do
+  @moduledoc """
+  Documentation for `Adventofcode2025`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adventofcode2025.hello()
+      :world
+
+  """
+  def hello do
+    :world
+
+    IO.puts("asd")
+  end
+end
---
+++ test/adventofcode2025_test.exs
@@ -0,0 +1,8 @@
+defmodule Adventofcode2025Test do
+  use ExUnit.Case
+  doctest Adventofcode2025
+
+  test "greets the world" do
+    assert Adventofcode2025.hello() == :world
+  end
+end
