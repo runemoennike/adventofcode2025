4f7f476af66841e6e5c78e11108f28fed772a04f
Rune A. Juel Mønnike <rajm@force.dk>
2025-12-06 22:42:59 +0100

    Shorter.

--- lib/day_01.ex
+++ lib/day_01.ex
@@ -2,30 +2,18 @@ defmodule Day01 do
   def parse(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&parse_one/1)
-  end
-
-  def parse_one(step) do
-    case step |> String.split_at(1) do
-      {"L", t} -> String.to_integer(t) * -1
-      {"R", t} -> String.to_integer(t)
-    end
+    |> Enum.map(&(String.trim(&1) |> String.split_at(1)))
+    |> Enum.map(fn
+      {"L", n} -> -String.to_integer(n)
+      {"R", n} -> String.to_integer(n)
+    end)
   end
 
   def part1(steps) do
     steps
     |> List.foldl({50, 0}, fn step, {position, zeroes} ->
       new_position = Integer.mod(position + step, 100)
-
-      {
-        new_position,
-        if new_position == 0 do
-          zeroes + 1
-        else
-          zeroes
-        end
-      }
+      {new_position, zeroes + if(new_position == 0, do: 1, else: 0)}
     end)
     |> elem(1)
   end
