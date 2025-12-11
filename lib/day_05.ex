78ce21dbf2a3dbdf1b79ca6f91cbc42781d4c2a8
Rune A. Juel Mønnike <rajm@force.dk>
2025-12-11 21:16:38 +0100

    Day 5 part 1.

---
+++ lib/day_05.ex
@@ -0,0 +1,25 @@
+defmodule Day05 do
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> Enum.split_while(&(&1 != ""))
+    |> then(fn {fresh, available} ->
+      {
+        fresh
+        |> Enum.map(fn s ->
+          s
+          |> String.split("-")
+          |> then(fn [from, to] -> String.to_integer(from)..String.to_integer(to) end)
+        end),
+        available |> Enum.drop(1) |> Enum.map(&String.to_integer/1)
+      }
+    end)
+  end
+
+  def part1(fresh, available) do
+    available
+    |> Enum.filter(fn i -> fresh |> Enum.any?(&(i in &1)) end)
+    |> Enum.count()
+  end
+end
---
+++ test/day_05_test.exs
@@ -0,0 +1,58 @@
+defmodule Day05Test do
+  use ExUnit.Case, async: true
+
+  import Day05
+
+  setup do
+    [
+      input: """
+      3-5
+      10-14
+      16-20
+      12-18
+
+      1
+      5
+      8
+      11
+      17
+      32
+      """
+    ]
+  end
+
+  test "parse", %{input: input} do
+    {fresh, available} = parse(input)
+
+    assert [3..5, 10..14, 16..20, 12..18] == fresh
+    assert [1, 5, 8, 11, 17, 32] == available
+  end
+
+  test "part1 example", %{input: input} do
+    result = input |> parse() |> then(fn {fresh, available} -> part1(fresh, available) end)
+
+    assert result == 3
+  end
+
+  test "part1" do
+    input = File.read!("./puzzle_input/day_05.txt")
+
+    result = input |> parse() |> then(fn {fresh, available} -> part1(fresh, available) end)
+
+    IO.puts("Part 1: #{result}")
+  end
+
+  # test "part2 example", %{input: input} do
+  #   result = input |> parse() |> part2()
+  #
+  #   assert result == 43
+  # end
+  #
+  # test "part2" do
+  #   input = File.read!("./puzzle_input/day_05.txt")
+  #
+  #   result = input |> parse() |> part2()
+  #
+  #   IO.puts("Part 2: #{result}")
+  # end
+end
