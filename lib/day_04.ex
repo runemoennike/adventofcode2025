379ca45637ae330ef60645f3e0b49eb8c7b058da
Rune A. Juel Mønnike <rajm@force.dk>
2025-12-11 13:10:16 +0100

    Day 4 part 2.

--- lib/day_04.ex
+++ lib/day_04.ex
@@ -2,48 +2,71 @@ defmodule Day04 do
   @type tile :: :none | :roll
   @type tile_map :: [[tile]]
 
+  @neighbours -1..1
+              |> Enum.flat_map(fn j -> for i <- -1..1, do: {i, j} end)
+              |> Enum.reject(&(&1 == {0, 0}))
+
   @spec parse(String.t()) :: tile_map
   def parse(input) do
     input
     |> String.trim()
     |> String.split("\n", trim: true)
-    |> Enum.map(&(String.trim(&1) |> parse_line))
-  end
-
-  @spec parse_line(String.t()) :: [tile]
-  def parse_line(line) do
-    line
-    |> String.graphemes()
-    |> Enum.map(fn
-      "." -> :none
-      "@" -> :roll
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, y} ->
+      line
+      |> String.trim()
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.map(fn
+        {".", x} -> {{x, y}, :none}
+        {"@", x} -> {{x, y}, :roll}
+      end)
     end)
+    |> Map.new()
   end
 
   @spec part1(tile_map) :: pos_integer()
   def part1(map) do
     map
-    |> Enum.with_index()
-    |> Enum.map(fn {line, y} ->
-      line
-      |> Enum.with_index()
-      |> Enum.map(fn
-        {:roll, x} -> count_free_neighbours(map, x, y)
-        {:none, _} -> 0
-      end)
-    end)
-    |> Enum.concat()
-    |> Enum.filter(&(&1 >= 5))
+    |> reachable_rolls()
     |> Enum.count()
   end
 
-  def count_free_neighbours(map, x, y) do
-    -1..1
-    |> Enum.flat_map(fn j -> for i <- -1..1, do: {i, j} end)
-    |> Enum.map(fn {dx, dy} -> free?(map, x + dx, y + dy) end)
-    |> Enum.count(& &1)
+  def reachable_rolls(map) do
+    map
+    |> Enum.filter(fn {_, v} -> v == :roll end)
+    |> Enum.map(fn {{x, y}, _} -> {{x, y}, count_free_neighbours(map, x, y)} end)
+    |> Enum.filter(fn {_, n} -> n >= 5 end)
   end
 
-  def free?(_map, x, y) when x < 0 or y < 0, do: true
-  def free?(map, x, y), do: get_in(map, [Access.at(y), Access.at(x)]) in [nil, :none]
+  def count_free_neighbours(map, x, y) do
+    @neighbours
+    |> Enum.filter(fn {dx, dy} -> free?(map, x + dx, y + dy) end)
+    |> Enum.count()
+  end
+
+  def free?(map, x, y), do: map[{x, y}] in [nil, :none]
+
+  @spec part2(tile_map) :: pos_integer()
+  def part2(tile_map) do
+    Stream.unfold(tile_map, &remove_rolls/1)
+    |> Enum.sum()
+  end
+
+  def remove_rolls(tile_map) do
+    reachable =
+      tile_map
+      |> reachable_rolls()
+
+    count = length(reachable)
+
+    new_map =
+      tile_map
+      |> Map.merge(Map.new(reachable, fn {k, _} -> {k, :none} end))
+
+    case count do
+      0 -> nil
+      _ -> {count, new_map}
+    end
+  end
 end
--- test/day_04_test.exs
+++ test/day_04_test.exs
@@ -23,18 +23,108 @@ defmodule Day04Test do
   test "parse", %{input: input} do
     map = parse(input)
 
-    assert [
-             [:none, :none, :roll, :roll, :none, :roll, :roll, :roll, :roll, :none],
-             [:roll, :roll, :roll, :none, :roll, :none, :roll, :none, :roll, :roll],
-             [:roll, :roll, :roll, :roll, :roll, :none, :roll, :none, :roll, :roll],
-             [:roll, :none, :roll, :roll, :roll, :roll, :none, :none, :roll, :none],
-             [:roll, :roll, :none, :roll, :roll, :roll, :roll, :none, :roll, :roll],
-             [:none, :roll, :roll, :roll, :roll, :roll, :roll, :roll, :none, :roll],
-             [:none, :roll, :none, :roll, :none, :roll, :none, :roll, :roll, :roll],
-             [:roll, :none, :roll, :roll, :roll, :none, :roll, :roll, :roll, :roll],
-             [:none, :roll, :roll, :roll, :roll, :roll, :roll, :roll, :roll, :none],
-             [:roll, :none, :roll, :none, :roll, :roll, :roll, :none, :roll, :none]
-           ] == map
+    assert %{
+             {0, 0} => :none,
+             {0, 1} => :roll,
+             {0, 2} => :roll,
+             {0, 3} => :roll,
+             {0, 4} => :roll,
+             {0, 5} => :none,
+             {0, 6} => :none,
+             {0, 7} => :roll,
+             {0, 8} => :none,
+             {0, 9} => :roll,
+             {1, 0} => :none,
+             {1, 1} => :roll,
+             {1, 2} => :roll,
+             {1, 3} => :none,
+             {1, 4} => :roll,
+             {1, 5} => :roll,
+             {1, 6} => :roll,
+             {1, 7} => :none,
+             {1, 8} => :roll,
+             {1, 9} => :none,
+             {2, 0} => :roll,
+             {2, 1} => :roll,
+             {2, 2} => :roll,
+             {2, 3} => :roll,
+             {2, 4} => :none,
+             {2, 5} => :roll,
+             {2, 6} => :none,
+             {2, 7} => :roll,
+             {2, 8} => :roll,
+             {2, 9} => :roll,
+             {3, 0} => :roll,
+             {3, 1} => :none,
+             {3, 2} => :roll,
+             {3, 3} => :roll,
+             {3, 4} => :roll,
+             {3, 5} => :roll,
+             {3, 6} => :roll,
+             {3, 7} => :roll,
+             {3, 8} => :roll,
+             {3, 9} => :none,
+             {4, 0} => :none,
+             {4, 1} => :roll,
+             {4, 2} => :roll,
+             {4, 3} => :roll,
+             {4, 4} => :roll,
+             {4, 5} => :roll,
+             {4, 6} => :none,
+             {4, 7} => :roll,
+             {4, 8} => :roll,
+             {4, 9} => :roll,
+             {5, 0} => :roll,
+             {5, 1} => :none,
+             {5, 2} => :none,
+             {5, 3} => :roll,
+             {5, 4} => :roll,
+             {5, 5} => :roll,
+             {5, 6} => :roll,
+             {5, 7} => :none,
+             {5, 8} => :roll,
+             {5, 9} => :roll,
+             {6, 0} => :roll,
+             {6, 1} => :roll,
+             {6, 2} => :roll,
+             {6, 3} => :none,
+             {6, 4} => :roll,
+             {6, 5} => :roll,
+             {6, 6} => :none,
+             {6, 7} => :roll,
+             {6, 8} => :roll,
+             {6, 9} => :roll,
+             {7, 0} => :roll,
+             {7, 1} => :none,
+             {7, 2} => :none,
+             {7, 3} => :none,
+             {7, 4} => :none,
+             {7, 5} => :roll,
+             {7, 6} => :roll,
+             {7, 7} => :roll,
+             {7, 8} => :roll,
+             {7, 9} => :none,
+             {8, 0} => :roll,
+             {8, 1} => :roll,
+             {8, 2} => :roll,
+             {8, 3} => :roll,
+             {8, 4} => :roll,
+             {8, 5} => :none,
+             {8, 6} => :roll,
+             {8, 7} => :roll,
+             {8, 8} => :roll,
+             {8, 9} => :roll,
+             {9, 0} => :none,
+             {9, 1} => :roll,
+             {9, 2} => :roll,
+             {9, 3} => :none,
+             {9, 4} => :roll,
+             {9, 5} => :roll,
+             {9, 6} => :roll,
+             {9, 7} => :roll,
+             {9, 8} => :none,
+             {9, 9} => :none
+           } == map
   end
 
   test "part1 example", %{input: input} do
@@ -49,5 +139,19 @@ defmodule Day04Test do
     result = input |> parse() |> part1()
 
     IO.puts("Part 1: #{result}")
+  end
+
+  test "part2 example", %{input: input} do
+    result = input |> parse() |> part2()
+
+    assert result == 43
+  end
+
+  test "part2" do
+    input = File.read!("./puzzle_input/day_04.txt")
+
+    result = input |> parse() |> part2()
+
+    IO.puts("Part 2: #{result}")
   end
 end
