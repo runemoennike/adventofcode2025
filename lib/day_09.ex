188562ad555faa3e5c44f12164acebd210b7fff1
Rune A. Juel Mønnike <rajm@force.dk>
2026-01-05 12:57:09 +0100

    Day 9 part 2 wip.

--- lib/day_09.ex
+++ lib/day_09.ex
@@ -21,38 +21,61 @@ defmodule Day09 do
 
   def part2(points) do
     edges = points |> polygon_edges()
-    lookup = points |> compress
-    map = lookup |> generate_map |> rasterise_edges(edges)
+    lookup = points |> compress |> IO.inspect()
+    map = lookup |> generate_map |> rasterise_edges(edges, lookup) |> IO.inspect()
   end
 
-  def rasterise_edges(map, edges) do
-    edges |> Enum.reduce(map, &rasterise_edge/2)
+  def rasterise_edges(map, edges, lookup) do
+    edges |> encode(lookup) |> Enum.reduce(map, fn edge, map -> rasterise_edge(map, edge) end)
   end
 
   def rasterise_edge(map, {{x, y1}, {x, y2}}) do
-    for y <- min(y1, y2)..max(y1..y2), do: y
-      |> Enum.reduce(map, fn y, map -> map
+    map
+    |> Enum.with_index()
+    |> Enum.map(fn
+      {_v, y} when y in y1..y2//1 -> map |> Enum.at(y) |> List.replace_at(x, 1)
+      {v, _y} -> v
+    end)
   end
 
   def rasterise_edge(map, {{x1, y}, {x2, y}}) do
-    nil
+    map
+    |> List.replace_at(
+      y,
+      map
+      |> Enum.at(y)
+      |> Enum.with_index()
+      |> Enum.map(fn
+        {_v, x} when x in x1..x2//1 -> 1
+        {v, _x} -> v
+      end)
+    )
   end
 
   def rasterise_edge(_map, {{_x1, _y1}, {_x2, _y2}}),
     do: raise("Can only rasterise horizontal and vertical edges.")
 
   def generate_map(lookup) do
-    # Row first order
+    # Row major order, i.e. map[y][x]
     0 |> List.duplicate(length(lookup.x)) |> List.duplicate(length(lookup.y))
   end
+
+  def encode([head | tail]), do: [encode(head) | encode(tail)]
+  def encode({{_, _} = a, {_, _} = b}), do: {encode(a), encode(b)}
+  def encode({x, y}, lookup), do: {lookup.x[x], lookup.y[y]}
 
   def compress(points) do
     points
     |> Enum.unzip()
     |> then(fn {xs, ys} ->
+      x = xs |> Enum.sort() |> Enum.dedup()
+      y = ys |> Enum.sort() |> Enum.dedup()
+
       %{
-        x: xs |> Enum.sort() |> Enum.dedup(),
-        y: ys |> Enum.sort() |> Enum.dedup()
+        x: x,
+        y: y,
+        x_rev: x |> Enum.with_index() |> Map.new(),
+        y_rev: y |> Enum.with_index() |> Map.new()
       }
     end)
   end
@@ -67,7 +90,7 @@ defmodule Day09 do
   end
 
   def polygon_edges(points) do
-    points |> Enum.zip(points.drop(1) ++ [hd(points)])
+    points |> Enum.zip(Enum.drop(points, 1) ++ [hd(points)])
   end
 
   def build_areas(positions) do
