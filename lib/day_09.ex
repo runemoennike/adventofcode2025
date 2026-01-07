389fd2930193ac262b53debc0f19f5bf5b20dfc0
Rune A. Juel Mønnike <rajm@force.dk>
2026-01-07 14:29:41 +0100

    Cleanup.

--- lib/day_09.ex
+++ lib/day_09.ex
@@ -1,6 +1,4 @@
 defmodule Day09 do
-  use Memoize
-
   @directions [{1, 0}, {-1, 0}, {0, 1}, {0, -1}]
 
   def parse(input) do
@@ -34,12 +32,6 @@ defmodule Day09 do
     |> then(fn {area, _a, _b} -> area end)
   end
 
-  def is_oob([], _map), do: false
-  def is_oob([h | t], map), do: is_oob(h, map) or is_oob(t, map)
-
-  def is_oob({x, y}, map) when is_integer(x) and is_integer(y),
-    do: get_in(map, [Access.at(y), Access.at(x)]) == 0
-
   def is_oob({{ax, ay} = a, {bx, by} = b}, map) do
     is_oob({ax, by}, map) or is_oob({bx, ay}, map) or
       get_edge_coordinates(a, b)
@@ -48,11 +40,14 @@ defmodule Day09 do
       end)
   end
 
+  def is_oob([], _map), do: false
+  def is_oob([h | t], map), do: is_oob(h, map) or is_oob(t, map)
+  def is_oob({x, y}, map), do: get_in(map, [Access.at(y), Access.at(x)]) == 0
+
   def get_edge_coordinates({x, y1}, {x, y2}) when y1 <= y2, do: for(y <- y1..y2//1, do: {x, y})
   def get_edge_coordinates({x, y1}, {x, y2}) when y1 > y2, do: for(y <- y2..y1//1, do: {x, y})
   def get_edge_coordinates({x1, y}, {x2, y}) when x1 <= x2, do: for(x <- x1..x2//1, do: {x, y})
   def get_edge_coordinates({x1, y}, {x2, y}) when x1 > x2, do: for(x <- x2..x1//1, do: {x, y})
-
   def get_edge_coordinates({_x1, _y1}, {_x2, _y2}), do: raise("Can only do rectilinear lines.")
 
   def print_map(map) do
@@ -86,25 +81,23 @@ defmodule Day09 do
     end)
   end
 
-  defp flood(map, x, y) do
-    cond do
-      x < 0 or y < 0 or y >= length(map) or x >= length(hd(map)) ->
-        {:fail, nil}
+  def flood(map, x, y) when x < 0 or y < 0 or y >= length(map) or x >= length(hd(map)),
+    do: {:fail, nil}
 
-      map |> Enum.at(y) |> Enum.at(x) == 1 ->
-        {:ok, map}
-
-      true ->
-        map
-        |> update_in([Access.at(y), Access.at(x)], fn _ -> 1 end)
-        |> then(fn map ->
-          Enum.reduce(@directions, {:ok, map}, fn {dx, dy}, {status, cur_map} ->
-            case flood(cur_map, x + dx, y + dy) do
-              {:fail, next_map} -> {:fail, next_map}
-              {:ok, next_map} -> {status, next_map}
-            end
-          end)
+  def flood(map, x, y) do
+    if map |> Enum.at(y) |> Enum.at(x) == 1 do
+      {:ok, map}
+    else
+      map
+      |> update_in([Access.at(y), Access.at(x)], fn _ -> 1 end)
+      |> then(fn map ->
+        Enum.reduce(@directions, {:ok, map}, fn {dx, dy}, {status, cur_map} ->
+          case flood(cur_map, x + dx, y + dy) do
+            {:fail, next_map} -> {:fail, next_map}
+            {:ok, next_map} -> {status, next_map}
+          end
         end)
+      end)
     end
   end
 
@@ -115,21 +108,16 @@ defmodule Day09 do
   end
 
   def rasterise_edge(map, {{x, y1}, {x, y2}}) do
-    ya = min(y1, y2)
-    yb = max(y1, y2)
-
     map
     |> Enum.with_index()
     |> Enum.map(fn
-      {_v, y} when y in ya..yb//1 -> map |> Enum.at(y) |> List.replace_at(x, 1)
+      {_v, y} when y1 <= y2 and y in y1..y2//1 -> map |> Enum.at(y) |> List.replace_at(x, 1)
+      {_v, y} when y1 > y2 and y in y2..y1//1 -> map |> Enum.at(y) |> List.replace_at(x, 1)
       {v, _y} -> v
     end)
   end
 
   def rasterise_edge(map, {{x1, y}, {x2, y}}) do
-    xa = min(x1, x2)
-    xb = max(x1, x2)
-
     map
     |> List.replace_at(
       y,
@@ -137,7 +125,8 @@ defmodule Day09 do
       |> Enum.at(y)
       |> Enum.with_index()
       |> Enum.map(fn
-        {_v, x} when x in xa..xb//1 -> 1
+        {_v, x} when x1 <= x2 and x in x1..x2//1 -> 1
+        {_v, x} when x1 > x2 and x in x2..x1//1 -> 1
         {v, _x} -> v
       end)
     )
@@ -154,32 +143,26 @@ defmodule Day09 do
   def encode([], _lookup), do: []
   def encode([head | tail], lookup), do: [encode(head, lookup) | encode(tail, lookup)]
   def encode({{_, _} = a, {_, _} = b}, lookup), do: {encode(a, lookup), encode(b, lookup)}
-  def encode({x, y}, lookup), do: {lookup.x_rev[x], lookup.y_rev[y]}
+  def encode({x, y}, lookup), do: {lookup.x[x], lookup.y[y]}
 
   def compress(points) do
     points
     |> Enum.unzip()
     |> then(fn {xs, ys} ->
-      x = xs |> Enum.sort() |> Enum.dedup()
-      y = ys |> Enum.sort() |> Enum.dedup()
-
       %{
-        x: x |> Enum.with_index() |> Map.new(fn {v, idx} -> {idx, v} end),
-        y: y |> Enum.with_index() |> Map.new(fn {v, idx} -> {idx, v} end),
-        x_rev: x |> Enum.with_index() |> Map.new(),
-        y_rev: y |> Enum.with_index() |> Map.new(),
-        valid_x: MapSet.new(x),
-        valid_y: MapSet.new(y)
+        x: xs |> Enum.sort() |> Enum.dedup() |> Enum.with_index() |> Map.new(),
+        y: ys |> Enum.sort() |> Enum.dedup() |> Enum.with_index() |> Map.new()
       }
     end)
   end
 
   def rect_edges({ax, ay}, {bx, by}) do
     [
-      # {{ax, ay}, {bx, ay}},
       {{bx, ay}, {bx, by}},
-      # {{bx, by}, {ax, by}},
       {{ax, by}, {ax, ay}}
+      # Horizontal lines not relevant in the given data set.
+      # {{ax, ay}, {bx, ay}},
+      # {{bx, by}, {ax, by}},
     ]
   end
 
@@ -187,10 +170,8 @@ defmodule Day09 do
     points |> Enum.zip(Enum.drop(points, 1) ++ [hd(points)])
   end
 
-  def build_areas(positions) do
-    positions
-    |> Enum.flat_map(fn pos -> build_areas(pos, positions) end)
-  end
+  def build_areas(positions),
+    do: positions |> Enum.flat_map(fn pos -> build_areas(pos, positions) end)
 
   def build_areas(pos, positions) do
     positions
@@ -198,7 +179,5 @@ defmodule Day09 do
     |> Enum.map(fn pos_b -> {inclusive_area(pos, pos_b), pos, pos_b} end)
   end
 
-  def inclusive_area({ax, ay}, {bx, by}) do
-    (abs(bx - ax) + 1) * (abs(by - ay) + 1)
-  end
+  def inclusive_area({ax, ay}, {bx, by}), do: (abs(bx - ax) + 1) * (abs(by - ay) + 1)
 end
