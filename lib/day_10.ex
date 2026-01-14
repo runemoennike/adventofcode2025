ee4c1a12abe5213b6a48b72ed8599399a5b2ec8a
Rune A. Juel Mønnike <rajm@force.dk>
2026-01-14 22:04:34 +0100

    Day 10 part 2 wip.

--- lib/day_10.ex
+++ lib/day_10.ex
@@ -8,10 +8,13 @@ defmodule Day10 do
       String.split(l, " ")
       |> Enum.map(fn s -> String.slice(s, 1, String.length(s) - 2) end)
       |> then(fn segments ->
+        {last, [first | middle]} = List.pop_at(segments, -1)
+
         %{
-          goal: parse_goal(hd(segments)),
-          actions: Enum.slice(segments, 1, length(segments) - 2) |> Enum.map(&parse_action/1),
-          reqs: parse_reqs(List.last(segments))
+          goal: parse_goal(first),
+          action_bitmaps: Enum.map(middle, &parse_action_bitmap/1),
+          action_vectors: Enum.map(middle, &parse_action_vector(&1, String.length(first))),
+          reqs: parse_reqs(last)
         }
       end)
     end)
@@ -27,40 +30,38 @@ defmodule Day10 do
     end)
   end
 
-  def parse_action(input) do
-    input
-    |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.sum_by(fn n -> 2 ** n end)
+  def parse_action_bitmap(input), do: integer_tokens(input) |> Enum.sum_by(fn n -> 2 ** n end)
+
+  def parse_action_vector(input, len) do
+    integer_tokens(input)
+    |> Enum.reduce(Tuple.duplicate(0, len), fn pos, vec -> vec |> put_elem(pos, 1) end)
   end
 
-  def parse_reqs(input) do
-    input
-    |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
-    |> List.to_tuple()
-  end
+  def parse_reqs(input), do: integer_tokens(input) |> List.to_tuple()
+
+  def integer_tokens(input), do: input |> String.split(",") |> Enum.map(&String.to_integer/1)
 
   def part1(problems) do
     problems
-    |> Enum.map(fn %{goal: goal, actions: actions} -> solve(goal, actions) |> hd() |> length() end)
-    |> Enum.sum()
+    |> Enum.sum_by(fn %{goal: goal, action_bitmaps: actions} ->
+      solve_bitmap(goal, actions) |> hd() |> length()
+    end)
   end
 
-  def solve(goal, actions) do
+  def solve_bitmap(goal, actions) do
     for(depth <- 1..length(actions), do: depth)
     |> Enum.reduce_while(nil, fn depth, _acc ->
-      case solve(0, goal, actions, [], depth) do
+      case solve_bitmap(0, goal, actions, [], depth) do
         [] -> {:cont, nil}
         solution -> {:halt, solution}
       end
     end)
   end
 
-  def solve(_state, _goal, [], _history, _depth), do: []
-  def solve(_state, _goal, _actions, _history, 0), do: []
+  def solve_bitmap(_state, _goal, [], _history, _depth), do: []
+  def solve_bitmap(_state, _goal, _actions, _history, 0), do: []
 
-  def solve(state, goal, actions, history, depth) do
+  def solve_bitmap(state, goal, actions, history, depth) do
     actions
     |> Enum.flat_map(fn action ->
       new_history = [action | history]
@@ -71,10 +72,178 @@ defmodule Day10 do
         [new_history]
       else
         new_actions = actions |> List.delete(action)
-        solve(new_state, goal, new_actions, new_history, new_depth)
+        solve_bitmap(new_state, goal, new_actions, new_history, new_depth)
       end
     end)
   end
 
-  def bits(n), do: for(<<(b::1 <- <<n>>)>>, do: b) |> Enum.join()
+  def part2(problems) do
+    problems
+    |> Enum.sum_by(fn %{reqs: goal, action_vectors: actions} ->
+      solve_vector(goal, actions)
+    end)
+  end
+
+  def solve_vector(goal, actions) do
+    IO.inspect(goal, label: "SOLVING -----------------")
+
+    for(depth <- 1..20, do: depth)
+    |> Enum.reduce_while(nil, fn depth, _acc ->
+      IO.inspect(depth)
+
+      case solve_vector(Tuple.duplicate(0, tuple_size(goal)), goal, actions, 0, depth) do
+        nil -> {:cont, nil}
+        solution -> {:halt, solution}
+      end
+    end)
+  end
+
+  def solve_vector({s1}, {g1}, _actions, _history, _depth) when s1 > g1, do: nil
+
+  def solve_vector({s1, s2}, {g1, g2}, _actions, _history, _depth) when s1 > g1 when s2 > g2,
+    do: nil
+
+  def solve_vector({s1, s2, s3}, {g1, g2, g3}, _actions, _history, _depth)
+      when s1 > g1
+      when s2 > g2
+      when s3 > g3,
+      do: nil
+
+  def solve_vector({s1, s2, s3, s4}, {g1, g2, g3, g4}, _actions, _history, _depth)
+      when s1 > g1
+      when s2 > g2
+      when s3 > g3
+      when s4 > g4,
+      do: nil
+
+  def solve_vector({s1, s2, s3, s4, s5}, {g1, g2, g3, g4, g5}, _actions, _history, _depth)
+      when s1 > g1
+      when s2 > g2
+      when s3 > g3
+      when s4 > g4
+      when s5 > g5,
+      do: nil
+
+  def solve_vector({s1, s2, s3, s4, s5, s6}, {g1, g2, g3, g4, g5, g6}, _actions, _history, _depth)
+      when s1 > g1
+      when s2 > g2
+      when s3 > g3
+      when s4 > g4
+      when s5 > g5
+      when s6 > g6,
+      do: nil
+
+  def solve_vector(
+        {s1, s2, s3, s4, s5, s6, s7},
+        {g1, g2, g3, g4, g5, g6, g7},
+        _actions,
+        _history,
+        _depth
+      )
+      when s1 > g1
+      when s2 > g2
+      when s3 > g3
+      when s4 > g4
+      when s5 > g5
+      when s6 > g6
+      when s7 > g7,
+      do: nil
+
+  def solve_vector(
+        {s1, s2, s3, s4, s5, s6, s7, s8},
+        {g1, g2, g3, g4, g5, g6, g7, g8},
+        _actions,
+        _history,
+        _depth
+      )
+      when s1 > g1
+      when s2 > g2
+      when s3 > g3
+      when s4 > g4
+      when s5 > g5
+      when s6 > g6
+      when s7 > g7
+      when s8 > g8,
+      do: nil
+
+  def solve_vector(
+        {s1, s2, s3, s4, s5, s6, s7, s8, s9},
+        {g1, g2, g3, g4, g5, g6, g7, g8, g9},
+        _actions,
+        _history,
+        _depth
+      )
+      when s1 > g1
+      when s2 > g2
+      when s3 > g3
+      when s4 > g4
+      when s5 > g5
+      when s6 > g6
+      when s7 > g7
+      when s8 > g8
+      when s9 > g9,
+      do: nil
+
+  def solve_vector(
+        {s1, s2, s3, s4, s5, s6, s7, s8, s9, s10},
+        {g1, g2, g3, g4, g5, g6, g7, g8, g9, g10},
+        _actions,
+        _history,
+        _depth
+      )
+      when s1 > g1
+      when s2 > g2
+      when s3 > g3
+      when s4 > g4
+      when s5 > g5
+      when s6 > g6
+      when s7 > g7
+      when s8 > g8
+      when s9 > g9
+      when s10 > g10,
+      do: nil
+
+  def solve_vector(_state, _goal, _actions, _history, 0), do: nil
+
+  def solve_vector(state, goal, actions, history, depth) do
+    actions
+    |> Enum.reduce_while(nil, fn action, _acc ->
+      new_history = history + 1
+      new_state = add(state, action)
+      new_depth = depth - 1
+
+      if new_state == goal do
+        {:halt, new_history}
+      else
+        case solve_vector(new_state, goal, actions, new_history, new_depth) do
+          nil -> {:cont, nil}
+          v -> {:halt, v}
+        end
+      end
+    end)
+  end
+
+  def add({a1}, {b1}), do: {a1 + b1}
+  def add({a1, a2}, {b1, b2}), do: {a1 + b1, a2 + b2}
+  def add({a1, a2, a3}, {b1, b2, b3}), do: {a1 + b1, a2 + b2, a3 + b3}
+  def add({a1, a2, a3, a4}, {b1, b2, b3, b4}), do: {a1 + b1, a2 + b2, a3 + b3, a4 + b4}
+
+  def add({a1, a2, a3, a4, a5}, {b1, b2, b3, b4, b5}),
+    do: {a1 + b1, a2 + b2, a3 + b3, a4 + b4, a5 + b5}
+
+  def add({a1, a2, a3, a4, a5, a6}, {b1, b2, b3, b4, b5, b6}),
+    do: {a1 + b1, a2 + b2, a3 + b3, a4 + b4, a5 + b5, a6 + b6}
+
+  def add({a1, a2, a3, a4, a5, a6, a7}, {b1, b2, b3, b4, b5, b6, b7}),
+    do: {a1 + b1, a2 + b2, a3 + b3, a4 + b4, a5 + b5, a6 + b6, a7 + b7}
+
+  def add({a1, a2, a3, a4, a5, a6, a7, a8}, {b1, b2, b3, b4, b5, b6, b7, b8}),
+    do: {a1 + b1, a2 + b2, a3 + b3, a4 + b4, a5 + b5, a6 + b6, a7 + b7, a8 + b8}
+
+  def add({a1, a2, a3, a4, a5, a6, a7, a8, a9}, {b1, b2, b3, b4, b5, b6, b7, b8, b9}),
+    do: {a1 + b1, a2 + b2, a3 + b3, a4 + b4, a5 + b5, a6 + b6, a7 + b7, a8 + b8, a9 + b9}
+
+  def add({a1, a2, a3, a4, a5, a6, a7, a8, a9, a10}, {b1, b2, b3, b4, b5, b6, b7, b8, b9, b10}),
+    do:
+      {a1 + b1, a2 + b2, a3 + b3, a4 + b4, a5 + b5, a6 + b6, a7 + b7, a8 + b8, a9 + b9, a10 + b10}
 end
--- test/day_10_test.exs
+++ test/day_10_test.exs
@@ -19,17 +19,38 @@ defmodule Day10Test do
     assert [
              %{
                goal: 0b0110,
-               actions: [0b1000, 0b1010, 0b0100, 0b1100, 0b0101, 0b0011],
+               action_bitmaps: [0b1000, 0b1010, 0b0100, 0b1100, 0b0101, 0b0011],
+               action_vectors: [
+                 {0, 0, 0, 1},
+                 {0, 1, 0, 1},
+                 {0, 0, 1, 0},
+                 {0, 0, 1, 1},
+                 {1, 0, 1, 0},
+                 {1, 1, 0, 0}
+               ],
                reqs: {3, 5, 4, 7}
              },
              %{
                goal: 0b01000,
-               actions: [0b11101, 0b01100, 0b10001, 0b00111, 0b11110],
+               action_bitmaps: [0b11101, 0b01100, 0b10001, 0b00111, 0b11110],
+               action_vectors: [
+                 {1, 0, 1, 1, 1},
+                 {0, 0, 1, 1, 0},
+                 {1, 0, 0, 0, 1},
+                 {1, 1, 1, 0, 0},
+                 {0, 1, 1, 1, 1}
+               ],
                reqs: {7, 5, 12, 7, 2}
              },
              %{
                goal: 0b101110,
-               actions: [0b011111, 0b011001, 0b110111, 0b000110],
+               action_bitmaps: [0b011111, 0b011001, 0b110111, 0b000110],
+               action_vectors: [
+                 {1, 1, 1, 1, 1, 0},
+                 {1, 0, 0, 1, 1, 0},
+                 {1, 1, 1, 0, 1, 1},
+                 {0, 1, 1, 0, 0, 0}
+               ],
                reqs: {10, 11, 11, 5, 10, 5}
              }
            ] == result
@@ -49,17 +70,25 @@ defmodule Day10Test do
     IO.puts("Part 1: #{result}")
   end
 
-  # test "part2 example", %{input: input} do
-  #   result = input |> parse() |> part2()
-  #
-  #   assert nil == result
-  # end
-  #
-  # test "part2" do
-  #   input = File.read!("./puzzle_input/day_10.txt")
-  #
-  #   result = input |> parse() |> part2()
-  #
-  #   IO.puts("Part 2: #{result}")
-  # end
+  test "tuple elementwise addition" do
+    for l <- 1..10 do
+      t = 1..l |> Enum.to_list() |> List.to_tuple()
+      s = 2..(l * 2)//2 |> Enum.to_list() |> List.to_tuple()
+      assert s == add(t, t)
+    end
+  end
+
+  test "part2 example", %{input: input} do
+    result = input |> parse() |> part2()
+
+    assert 33 == result
+  end
+
+  test "part2" do
+    input = File.read!("./puzzle_input/day_10.txt")
+
+    result = input |> parse() |> part2()
+
+    IO.puts("Part 2: #{result}")
+  end
 end
